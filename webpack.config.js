a6be03c23d70542b937a5844a349db7f3efad850
Yin <15297927693@163.com>
2022-07-02 00:23:59 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin'); //通过 npm 安装
+const { Template } = require('webpack');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js'
-  }
+    filename: 'index.[contenthash].js'
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title:"晶晶的网站",
+    template :'src/assets/index.html'
+  })],
+  module: {
+    rules: [
+      { 
+        test: /\.css$/i, 
+        use:  ['style-loader','css-loader'],
+    },
+    ],
+  },
 };
